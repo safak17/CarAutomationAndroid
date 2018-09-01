3f56005e1a9980d7bc16cb11144b019fcd25a28b
Şafak AKINCI <safakakinci17@gmail.com>
2018-09-01 14:44:06 +0300

    UI is created.

---
+++ app/src/main/java/com/automation/CarAutomation/View/Activity/AlarmActivity.java
@@ -0,0 +1,15 @@
+package com.automation.CarAutomation.View.View.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.automation.CarAutomation.R;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
---
+++ app/src/main/java/com/automation/CarAutomation/View/Activity/PairedDevicesActivity.java
@@ -0,0 +1,15 @@
+package com.automation.CarAutomation.View.View.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.automation.CarAutomation.R;
+
+public class PairedDevicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paired_devices);
+    }
+}
